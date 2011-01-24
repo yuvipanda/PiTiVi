cf747ffc3683be07ff43ce932ce5c11367390322
Yuvaraj Pandian T <yuvipanda@gmail.com>
2011-01-24 20:54:37 +0530

    Added VuMeter from gst-python samples

--- pitivi/ui/audiorecord.py
+++ pitivi/ui/audiorecord.py
@@ -4,16 +4,11 @@ import os
 import gst
 import gtk
 
+from fvumeter import FVUMeter
 ui = """
  <interface>
   <requires lib="gtk+" version="2.16"/>
-      <object class="GtkProgressBar" id="vumeter">
-        <property name="visible">True</property>
-        <property name="sensitive">False</property>
-        <property name="show_text">True</property>
-        <property name="text" translatable="yes">Microphone</property>
-      </object>
-      <object class="GtkButton" id="record_button">
+     <object class="GtkButton" id="record_button">
         <property name="visible">True</property>
         <property name="sensitive">False</property>
         <property name="can_focus">True</property>
@@ -29,16 +24,26 @@ ui = """
       </interface>
 """
 
+def clamp(x, min, max):
+    if x < min:
+        return min
+    elif x > max:
+        return max
+    return x
+
 class AudioRecorder(gtk.HBox):
     def __init__(self, instance):
         gtk.HBox.__init__(self)
         self.audiosrc = gst.element_factory_make("autoaudiosrc", "audiosrc")
+        self.level = gst.element_factory_make("level", "level")
         self.encoder = gst.element_factory_make("flacenc", "encoder")
         self.sink = gst.element_factory_make("fdsink", "sink")
 
+        self.level.set_property("message", True)
+
         self.pipeline = gst.Pipeline()
-        self.pipeline.add(self.audiosrc, self.encoder, self.sink)
-        gst.element_link_many (self.audiosrc, self.encoder, self.sink)        
+        self.pipeline.add(self.audiosrc, self.level, self.encoder, self.sink)
+        gst.element_link_many (self.audiosrc, self.level, self.encoder, self.sink)        
 
         self.app = instance
         self._createUI()
@@ -51,14 +56,28 @@ class AudioRecorder(gtk.HBox):
 
         self.record_button = builder.get_object("record_button")
         self.button_image = builder.get_object("button_image")
-        self.vumeter = builder.get_object("vumeter")
+        self.vumeter = FVUMeter() 
 
         self.pack_start(self.record_button, expand=False, fill=False)
-        self.pack_start(self.vumeter, expand=True, fill=True)
+
+        eventbox = gtk.EventBox()
+        eventbox.add(self.vumeter)
+        self.pack_start(eventbox, expand=True, fill=True)
         self.set_size_request(300, -1)
 
         self.record_button.set_sensitive(True)
         self.record_button.connect("clicked", self._recordCb)
+
+    def _on_message(self, bus, message):
+        if message.structure.get_name() == 'level':
+            s = message.structure
+            self.vumeter.freeze_notify()
+            decay = clamp(s['decay'][0], -90.0, 0.0)
+            peak = clamp(s['peak'][0], -90.0, 0.0)
+
+            self.vumeter.set_property('decay', decay)
+            self.vumeter.set_property('peak', peak)
+        return True
 
     def _recordCb(self, widget):
         if not self.is_recording:
@@ -73,9 +92,17 @@ class AudioRecorder(gtk.HBox):
     def start_recording(self):
         self._tempfd, self._temppath = tempfile.mkstemp()
         self.sink.set_property("fd", self._tempfd)
+        self.pipeline.get_bus().add_signal_watch()
+        self._cb_id = self.pipeline.get_bus().connect("message::element", self._on_message)
         self.pipeline.set_state(gst.STATE_PLAYING)
 
     def stop_recording(self):
+        self.pipeline.get_bus().disconnect(self._cb_id)
+        self.pipeline.get_bus().remove_signal_watch()
         self.pipeline.set_state(gst.STATE_NULL)
         os.close(self._tempfd) # assuming gstreamer doesn't close the fd. Should check
         self.app.current.sources.addUris(['file:///' + self._temppath])
+
+    def show_all(self):
+        HBox.show_all(self)
+        self.vumeter.show()
